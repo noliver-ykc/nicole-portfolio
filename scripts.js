74917d85e70f51d0e27417ca327f472feb12c8d2
Nicole Olliver <noliver.ykc@gmail.com>
2022-11-03 18:35:30 +0900

    tried to make mobile nav

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+// scrolling changes image
 var initialSrc = "/assets/avatar.png";
 var scrollSrc = "/assets/avatar-scroll.png";
 
@@ -10,3 +12,18 @@ function myFunction() {
     document.getElementById("main-avatar-img").src = initialSrc;
   }
 }
+
+// mobile nav opener
+const menuBtn = document.querySelector('.mobile-nav');
+let menuOpen = false;
+
+// ↓ can probably be refactored using toggle
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
