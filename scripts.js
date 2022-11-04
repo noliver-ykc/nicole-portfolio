c4a6a3a1503f555cdaccf0cf2d249caf030e01b5
Nicole Olliver <noliver.ykc@gmail.com>
2022-11-04 18:03:50 +0900

    added mobile media queries for landing page on small devices

--- scripts.js
+++ scripts.js
@@ -3,10 +3,21 @@ var scrollSrc = "/assets/avatar-scroll.png";
 
 window.onscroll = function() {myFunction()};
 
+//medium devices up
+
 function myFunction() {
   if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
     document.getElementById("main-avatar-img").src = scrollSrc;
   } else {
     document.getElementById("main-avatar-img").src = initialSrc;
+  }
+}
+
+// phones etc
+function myFunction() {
+  if (document.body.scrollTop > 1|| document.documentElement.scrollTop > 1) {
+    document.getElementById("main-avatar-img-mobile").src = scrollSrc;
+  } else {
+    document.getElementById("main-avatar-img-mobile").src = initialSrc;
   }
 }
